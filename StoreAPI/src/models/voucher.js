c2e36ef01f102c7f0e2cf9464b043078850f4803
conghuy <huy534876@gmail.com>
2025-10-16 16:10:29 +0700

    vocher

---
+++ StoreAPI/src/models/voucher.js
@@ -0,0 +1,23 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class Voucher extends Model {
+        static associate(models) {
+            Voucher.belongsTo(models.TypeVoucher, { foreignKey: 'typeVoucherId', targetKey: 'id', as: 'typeVoucherOfVoucherData' })
+            Voucher.hasMany(models.OrderProduct, { foreignKey: 'voucherId', as: 'voucherData' })
+        }
+    };
+    Voucher.init({
+        fromDate: DataTypes.STRING,
+        toDate: DataTypes.STRING,
+        typeVoucherId: DataTypes.INTEGER,
+        amount: DataTypes.INTEGER,
+        codeVoucher: DataTypes.STRING
+    }, {
+        sequelize,
+        modelName: 'Voucher',
+    });
+    return Voucher;
+};
